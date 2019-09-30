ff0972ec29f75169cd7e7d95635ec34be29511ec
joseph-gansukh <g.sukhbat@gmail.com>
2019-09-30 16:05:03 -0500

    Added homepage, login, and sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.valid?
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Thank you for signing up!"
+      else
+        redirect_to signup_path
+      end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :bio, :email, :password, :password_confirmation)
+    end
+end
