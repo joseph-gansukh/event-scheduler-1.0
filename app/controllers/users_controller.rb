a8670d156a33b5cb4bd525864e866837933d1293
Avinash Sarguru <avinashsarguru@gmail.com>
2019-10-01 09:44:22 -0500

    sign up errors fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
 
     def create
       @user = User.new(user_params)
-      if @user.valid?
-        @user.save
+      if @user.save
         session[:user_id] = @user.id
         redirect_to user_path(@user), notice: "Thank you for signing up!"
       else
+        flash[:errors] = @user.errors.full_messages
         redirect_to signup_path
       end
     end
